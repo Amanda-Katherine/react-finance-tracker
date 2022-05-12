1b6747768532df4df04aaaf45313814bf6f89676
Amanda 'Leia' Brubaker <abrubaker-consultant@scholastic.com>
2022-05-12 14:41:55 -0400

    add ability to filter expenses by year via dropdown

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -7,17 +7,31 @@ import Card from '../UI/Card';
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState('2020');
 
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
   const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
-    console.log(filteredYear);
   };
+
+  let expensesContent = <p>No expenses found.</p>;
+
+  if (filteredExpenses.length > 0) {
+    expensesContent = filteredExpenses.map((expense) => (
+      <ExpenseItem
+        key={expense.id}
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+      />
+    ));
+  }
 
   return (
     <Card className="expenses">
       <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-      {items.map((expense) => (
-        <ExpenseItem key={expense.id} title={expense.title} amount={expense.amount} date={expense.date} />
-      ))}
+      { expensesContent }
     </Card>
   );
 };
