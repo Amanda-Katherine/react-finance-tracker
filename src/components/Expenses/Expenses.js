28cf6d221815cc72d6f0c53c698033d1caf27510
Amanda 'Leia' Brubaker <abrubaker-consultant@scholastic.com>
2022-05-12 14:57:00 -0400

    remove expenses list logic/content from Expenses.js and create new ExpensesList.js file to handle this

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,8 +1,8 @@
 import { useState } from 'react';
-import ExpenseItem from './ExpenseItem';
 import ExpensesFilter from './ExpensesFilter';
 import './Expenses.css';
 import Card from '../UI/Card';
+import ExpensesList from './ExpensesList';
 
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState('2020');
@@ -15,23 +15,10 @@ const Expenses = (props) => {
     setFilteredYear(selectedYear);
   };
 
-  let expensesContent = <p>No expenses found.</p>;
-
-  if (filteredExpenses.length > 0) {
-    expensesContent = filteredExpenses.map((expense) => (
-      <ExpenseItem
-        key={expense.id}
-        title={expense.title}
-        amount={expense.amount}
-        date={expense.date}
-      />
-    ));
-  }
-
   return (
     <Card className="expenses">
       <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-      { expensesContent }
+      <ExpensesList items={filteredExpenses}/>
     </Card>
   );
 };
